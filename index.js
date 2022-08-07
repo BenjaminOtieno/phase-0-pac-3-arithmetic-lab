d988cb2d38e5b81a33d430126f15e8c5d5e73929
BenjaminOtieno <benjamin.otieno@moringaschool.com>
2022-08-07 13:17:34 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(x, y) {
+    return x + y;
+}
+function subtract(x, y) {
+    return x - y;
+}
+function multiply(x, y){
+    return x * y;
+
+}
+function divide(x, y) {
+    return x / y;
+
+}
+function increment(n) {
+    return ++n
+}
+function decrement(n) {
+    return --n
+}
+function makeInt(n) {
+    return parseInt(n, 10)
+}
+function preserveDecimal(n) {
+    return parseFloat(n)
+}
+console.log(add(1, 80))
+console.log(subtract(60, 40))
+console.log(multiply(2, 3.4))
+console.log(divide(5.0, 2.5))
